7daf4305c5af0dbf7b29dd170038fee9772bdb17
Mmaud <maudlemao@gmail.com>
2020-09-21 15:20:40 +0200

    include error cases in getLowerNumber()

--- notions_de_base/exercice2.js
+++ notions_de_base/exercice2.js
@@ -1,6 +1,9 @@
-let getLowerNumber = (arrayOfNumbers) => {
-    let lowerNumber = null;
+const getLowerNumber = (arrayOfNumbers) => {
+    if (Array.isArray(arrayOfNumbers) === false) throw 'Parameter is not an array'
+    else if (arrayOfNumbers.length === 0) throw 'The array is empty'
+    let lowerNumber = arrayOfNumbers[0];
     arrayOfNumbers.forEach(element => {
+        if (isNaN(element) === true) throw 'Your array contains strings'
         lowerNumber = element < lowerNumber ? element : lowerNumber;
     });
     return lowerNumber;
