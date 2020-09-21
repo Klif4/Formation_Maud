7aa3cb0753091e52af3eb00db4f34a9315108214
Mmaud <maudlemao@gmail.com>
2020-09-21 17:26:07 +0200

    placing tools methods in the bottom of the file

--- notions_de_base/exercice2.js
+++ notions_de_base/exercice2.js
@@ -1,14 +1,3 @@
-const isAnArray = (array) => {
-    return Array.isArray(array);
-}
-const isEmpty = (array) => {
-    return array.length === 0;
-}
-const hasNotOnlyNumbers = (array) => {
-    const isNotANumber = (element) => isNaN(element);
-    return array.some(isNotANumber);
-}
-
 const getLowerNumber = (arrayOfNumbers) => {
     if (!isAnArray(arrayOfNumbers)) throw 'Parameter is not an array'
     if (isEmpty) throw 'The array is empty';
@@ -18,4 +7,15 @@ const getLowerNumber = (arrayOfNumbers) => {
         lowerNumber = element < lowerNumber ? element : lowerNumber;
     });
     return lowerNumber;
+}
+
+const isAnArray = (array) => {
+    return Array.isArray(array);
+}
+const isEmpty = (array) => {
+    return array.length === 0;
+}
+const hasNotOnlyNumbers = (array) => {
+    const isNotANumber = (element) => isNaN(element);
+    return array.some(isNotANumber);
 }
