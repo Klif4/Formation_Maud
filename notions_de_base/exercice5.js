76b9c0464c09dc7aff3879b5ec2d3fde1079a2f5
Mmaud <maudlemao@gmail.com>
2020-09-23 14:42:18 +0200

    creating getCardSortByQuantities(card)

---
+++ notions_de_base/exercice5.js
@@ -0,0 +1,15 @@
+const getCardSortByQuantities = (card) => {
+    if (hasNoValidQuantity(card)) throw "invalid quantity"
+    card.sort(function (objectActual, objectToCompare) {
+        return objectActual.quantity - objectToCompare.quantity
+    })
+    return card;
+}
+
+const hasNoValidQuantity = (array) => {
+    let result = false;
+    array.forEach(element => {
+        if (element.quantity === null || typeof(element.quantity) === "string" || isNaN(element.quantity)) result = true;
+    });
+    return result; 
+}
