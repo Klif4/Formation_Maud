ef04a0e2dd042728c9c6e544fa531eafc70b6212
Mmaud <maudlemao@gmail.com>
2020-09-23 11:00:33 +0200

    dissociate treatment and checking + correcting error messages

--- notions_de_base/exercice4.js
+++ notions_de_base/exercice4.js
@@ -1,9 +1,8 @@
-const getTotalPrice = (card) => {
+const getTheBill = (card) => {
     if (isEmpty(card)) throw "Your card is empty";
+    if (hasNoValidPriceOrQuantity(card)) throw "Price or quantity missing on one object";
     let finalBill = 0;
     card.map(object => {
-        if (hasNoPriceOrQuantity(object.quantity)) throw "the object " + object.item.name + ' has no quantity, your card has been cancelled';
-        if (hasNoPriceOrQuantity(object.item.price)) throw "the object " + object.item.name + ' has no price, your card has been cancelled';
         finalBill += object.quantity * object.item.price;
     })
     return finalBill;
@@ -12,6 +11,43 @@ const getTotalPrice = (card) => {
 const isEmpty = (array) => {
     return array.length === 0;
 }
-const hasNoPriceOrQuantity = (objectProperty) => {
-    return objectProperty === null || objectProperty === undefined || isNaN(objectProperty);
+
+const hasNoValidPriceOrQuantity = (array) => {
+    let result = false;
+    array.forEach(object => {
+        if (isNaN(object.quantity) || isNaN(object.item.price) || object.quantity === null || object.item.price === null || typeof(object.quantity) === "string" || typeof(object.item.price) === "string") {
+            result = true;
+        };
+    });
+    return result;
+}
+
+card = [
+    {
+        quantity: "kjkj",
+        item: {
+            name: 'chaise',
+            price: 10,
+        }
+    },
+    {
+        quantity: 2,
+        item: {
+            name: 'table',
+            price: 50,
+        }
+    },
+    {
+        quantity: 1,
+        item: {
+            name: 'canape',
+            price: 200,
+        }
+    }
+]
+
+try {
+    console.log(getTheBill(card));
+} catch (error) {
+    console.log(error);
 }
